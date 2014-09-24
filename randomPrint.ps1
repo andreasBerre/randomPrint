107e4b439885d470b2c19dcc369e4a9152c1bcda
Andreas Berre <andreas.berre@gmail.com>
2014-09-24 13:40:41 +0200

    Added func for printing, and better input handling

--- randomPrint.ps1
+++ randomPrint.ps1
@@ -1,42 +1,5 @@
 ﻿
-
-$documents = @()
-do {
-    $docPath = Read-Host 'Enter the path of the document you would like to print (e.g. "c:/document.docx")'
-    Write-Host $docPath
-    if (-not (Test-Path $docPath)) 
-    {
-        #throw [System.IO.FileNotFoundException] "$docPath not found."
-    }
-    $docName = Split-Path -Leaf $docPath
-    $numOfCopies = Read-Host 'How many copies do you want of' $docName '(e.g. "100")'
-    $moreDocs = Read-Host 'More documents? (y/n)' 
-
-    $document = New-Object System.Object
-    $document | Add-Member -type NoteProperty -name Path -value $docPath
-    $document | Add-Member -type NoteProperty -name NumOfCopies -value $numOfCopies
-    
-    for($i=1 
-        $numOfCopies -le 
-        $i++)
-        {
-        Write-Host $i
-        }
-}
-while ($moreDocs -like 'y')
-
-foreach ($document in $documents)
-{
-    Write-Host $document.Path $document.NumOfCopies
-}
-
-Get-ShuffledArray
-
-
-
-
-
-Function Get-ShuffledArray
+Function Shuffle
 {
     param([Array]$gnArr)
     
@@ -49,4 +12,46 @@ Function Get-ShuffledArray
         $gnArr[$i] = $tmp;
     }
     return $gnArr
+}
+
+$documents = @()
+do {
+    $docPath = Read-Host 'Enter the path of the document you would like to print (e.g. "c:/document.docx")'
+    Write-Host $docPath
+    if (-not (Test-Path $docPath)) 
+    {
+        Throw "$docPath was not found. Please check if the path is correct."
+    }
+    $docName = Split-Path -Leaf $docPath
+    $numOfCopies = Read-Host 'How many copies do you want of' $docName '(e.g. "100")'
+    if (-not $numOfCopies -is [int]) 
+    {
+        Throw "Number of copies must be an integer"
+    }
+         
+    for($i=1 
+        $i -le $numOfCopies
+        $i++)
+        {            
+            $document = New-Object System.Object
+            $document | Add-Member -type NoteProperty -name Path -value $docPath
+            $document | Add-Member -type NoteProperty -name Name -value $docName
+            $document | Add-Member -type NoteProperty -name NumOfCopies -value $numOfCopies
+            $document | Add-Member -type NoteProperty -name CopyNumber -value $i
+            $documents += $document
+        }
+    
+    do {
+        $moreDocs = Read-Host 'More documents? (y/n)'
+    } 
+    while ($moreDocs -notlike 'y' -and $moreDocs -notlike 'n')
+}
+while ($moreDocs -like 'y')
+
+$shuffledDocuments = Shuffle $documents
+
+foreach ($document in $shuffledDocuments)
+{
+    Write-Host 'printing: ' $document.Name', copy' $document.CopyNumber 'of' $document.NumOfCopies
+    #Start-Process -FilePath $document.Path -Verb print
 }
